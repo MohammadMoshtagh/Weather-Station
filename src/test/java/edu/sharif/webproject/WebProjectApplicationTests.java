221da7c32767ae2e33579ddb2313c52ee104d714
Mohammad <mhmmoshtaghi@gmail.com>
2024-05-12 16:44:34 +0330

    Initilize project

---
+++ src/test/java/edu/sharif/webproject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sharif.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
