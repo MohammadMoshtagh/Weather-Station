509b6f02496167883e438bb80ad69616c9150aaa
mohammad <mohammad.mo@smartech.ir>
2024-07-05 12:45:09 +0330

    add bdd testing

---
+++ weather/src/test/java/edu/sharif/webproject/weather/bdd/TestConfiguration.java
@@ -0,0 +1,9 @@
+package edu.sharif.webproject.weather.bdd;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ="edu.sharif.webproject.weather.bdd")
+class TestConfiguration {
+}
